f5028b29b17349f301e290294dbef7b09588474f
Ievgen Tovtin <zhe.tovtin@gmail.com>
2023-03-07 07:44:36 -0600

    added function declaration

--- README.md
+++ README.md
@@ -14,4 +14,5 @@ Terminal:
 -  desable parameter hints in VSCode settings 
 - Ctrl + Shift + K => removes current line where is cursor
 - pwd (Print Working Directory) => prints the route
-- Cntl + J => open terminal
+- Ctrl + J => open terminal
+- Shift + Alt + bottom arrow => current line code copy
--- main.py
+++ main.py
@@ -1,43 +1,4 @@
-print('Hello World!')
+def my_name(name):
+    print(name)
 
-list [1, 'abc', True]
-
-print("10, 'Ievgen', True => ")
-print(10, 'Ievgen', True)
-
-""" console print itselfe """
-
-print(print)
-
-""" console dir() as a function """
-print('print(dir()) => ')
-print(dir())
-
-""" console  """
-print('print(dir(__builtins__)) => list of all functions ---> ')
-print(dir(__builtins__))
-
-""" input(Enter your name) """
-input("Enter your name: ")
-
-""" input("Enter your name: ") """
-name = input("Enter your name: ")
-
-print('name => ')
-
-print(name)
-
-print('name.capitalize() => ')
-print(name.capitalize())
-
-print('name without calling capitalize function => ')
-print(name)
-
-print('name.upper() => ')
-print(name.upper())
-
-print('all attributes of "name" object => ')
-print(dir(name))
-
-
-
+my_name('Ievgen')
