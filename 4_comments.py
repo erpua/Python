674f215afe9cc3733175512a1910f849eef749d4
Ievgen Tovtin <zhe.tovtin@gmail.com>
2023-03-08 08:48:02 -0600

    added comments

---
+++ 4_comments.py
@@ -0,0 +1,14 @@
+# lines comments
+
+print(10)  # Inline comment. Add 2 spaces after code
+
+# WindowsErrorwer
+# enumerate
+# fghfgh
+
+
+def my_fn():
+    print('String')  # Comment
+
+
+print('String')      # Comment
