7a15b93bd3c0164e11c56c2fa4efbaa3a4c90894
Ievgen Tovtin <zhe.tovtin@gmail.com>
2023-03-13 13:59:00 -0600

    added changes

---
+++ 9_static_n_dynamic_types.py
@@ -0,0 +1,32 @@
+# Static
+# String a = 'Hello'
+
+# int b = 10
+
+# Dynamic: Python
+a = 'xyz'
+
+a = 10
+
+
+def print_name(name):
+    print(name)
+
+
+print_name('Ievgen')
+# Ievgen
+
+print_name = 10
+
+print_name('Ievgen')
+# TypeError: 'int' object is not callable
+
+# Objects: what it consists?
+name
+comments
+new_photos
+
+# Functions: what it does?
+get_data
+merge_names
+create_request
