0d9d2344b03053f810910931db05eb53e7fcc93a
Ievgen Tovtin <zhe.tovtin@gmail.com>
2023-03-13 19:06:49 -0600

    added integer methods

---
+++ 14_integers.py
@@ -0,0 +1,50 @@
+""" 
+frinds_quantity = 50
+
+print(frinds_quantity)
+# 50
+
+any_num = input('Enter any number: ')  # input returns str
+
+print(any_num)
+# number we entered
+
+print(type(any_num))
+# <class 'str'>' 
+
+"""
+
+""" 
+user_input = input('Enter number: ')
+any_num = int(user_input)
+
+print(any_num)
+# number we entered
+
+print(type(any_num))
+# < class 'int'> 
+"""
+
+""" 
+any_num = int(input('Enter number'))
+
+print(any_num)
+print(type(any_num))
+# < class 'int'> 
+"""
+
+# pow
+
+base = 5
+power = 3
+
+print(pow(base, power))
+# 5**3 => 125
+
+one_million = 1_000_000
+print(one_million)
+# 1000000
+
+my_number = 2_128
+print(my_number)
+# 2128
